980991fe4f280834ffdb3b3d8548561f92ec4239
felk <de.felix.koenig@gmail.com>
2021-05-04 22:16:29 +0200

    fix RoundToIncrement not actually rounding

--- AvaloniaOutseekClient/AvaloniaOutseekClient/Utils/MathUtils.cs
+++ AvaloniaOutseekClient/AvaloniaOutseekClient/Utils/MathUtils.cs
@@ -4,7 +4,13 @@ namespace AvaloniaOutseekClient.Utils
 {
     public static class MathUtils
     {
-        public static double RoundToIncrement(double val, double increment) =>
-            val - Math.IEEERemainder(val, increment);
+        public static double RoundToIncrement(double val, double increment)
+        {
+            if (increment <= 0) throw new ArgumentException("increment must be positive", nameof(increment));
+            double remainder = Math.IEEERemainder(val, increment);
+            return remainder < increment / 2
+                ? val - remainder
+                : val + remainder;
+        }
     }
 }
