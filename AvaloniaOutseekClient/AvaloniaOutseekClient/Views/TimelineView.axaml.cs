badfabdc34c3fc7235eb32a8d29a18e7c7938b35
felk <de.felix.koenig@gmail.com>
2021-05-04 22:24:50 +0200

    add basic timeline view with temp layout and test data

---
+++ AvaloniaOutseekClient/AvaloniaOutseekClient/Views/TimelineView.axaml.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using AvaloniaOutseekClient.SharedViewModels;
+using AvaloniaOutseekClient.ViewModels;
+
+namespace AvaloniaOutseekClient.Views
+{
+    public class TimelineView : UserControl
+    {
+        public TimelineView()
+        {
+            InitializeComponent();
+            DataContext = new TimelineViewModel(TimelineState.Instance);
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
+
