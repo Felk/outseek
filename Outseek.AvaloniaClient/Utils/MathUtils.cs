818319ec53bab7115be006e95c6e1478f39147ef
felk <de.felix.koenig@gmail.com>
2021-05-09 19:42:04 +0200

    allow zero increment in RoundToIncrement

--- Outseek.AvaloniaClient/Utils/MathUtils.cs
+++ Outseek.AvaloniaClient/Utils/MathUtils.cs
@@ -6,7 +6,8 @@ namespace Outseek.AvaloniaClient.Utils
     {
         public static double RoundToIncrement(double val, double increment)
         {
-            if (increment <= 0) throw new ArgumentException("increment must be positive", nameof(increment));
+            if (increment < 0) throw new ArgumentException("increment must be positive", nameof(increment));
+            if (increment == 0) return val;
             double remainder = Math.IEEERemainder(val, increment);
             return remainder < increment / 2
                 ? val - remainder
