11dcc3d3a6ced74d51898b66371a900db5417e52
felk <de.felix.koenig@gmail.com>
2021-05-13 18:35:50 +0200

    re-run timeline processors on start/end changes
    Quick & dirty way of refreshing the segments when the video gets swapped.
    Good for now for testing.

--- Outseek.AvaloniaClient/ViewModels/TimelineSegmentViewModel.cs
+++ Outseek.AvaloniaClient/ViewModels/TimelineSegmentViewModel.cs
@@ -3,34 +3,46 @@ using Outseek.API;
 using Outseek.API.Processors;
 using Outseek.AvaloniaClient.SharedViewModels;
 using Outseek.AvaloniaClient.ViewModels.TimelineObjects;
+using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
 
 namespace Outseek.AvaloniaClient.ViewModels
 {
     public class TimelineSegmentViewModel : ViewModelBase
     {
-        public TimelineState TimelineState { get; }
-        
-        [Reactive] public string? Text { get; set; }
+        private readonly ITimelineProcessor _timelineProcessor;
 
-        private ViewModelBase TimelineObject { get; }
+        public TimelineState TimelineState { get; }
+
+        [Reactive] public string? Text { get; set; }
+        [Reactive] private ViewModelBase? TimelineObject { get; set; }
 
         public TimelineSegmentViewModel() : this(new TimelineState(), new RandomSegmentsProcessor())
         {
             // the default constructor is only used by the designer
         }
 
-        public TimelineSegmentViewModel(TimelineState timelineState, ITimelineProcessor processor)
+        private void RerunProcessor()
         {
-            TimelineState = timelineState;
-            var context = new TimelineProcessContext(timelineState.Start, timelineState.End);
-            TimelineObject = processor.Process(context, new TimelineObject.Nothing()) switch
+            var context = new TimelineProcessContext(TimelineState.Start, TimelineState.End);
+            TimelineObject = _timelineProcessor.Process(context, new TimelineObject.Nothing()) switch
             {
                 TimelineObject.Nothing nothing => new NothingViewModel(),
                 TimelineObject.Segments segments => new SegmentsViewModel(TimelineState, segments),
                 _ => throw new ArgumentOutOfRangeException()
             };
-            Text = processor.Name;
+        }
+
+        public TimelineSegmentViewModel(
+            TimelineState timelineState, ITimelineProcessor processor)
+        {
+            TimelineState = timelineState;
+            _timelineProcessor = processor;
+            Text = _timelineProcessor.Name;
+            RerunProcessor();
+            timelineState
+                .WhenAnyValue(t => t.Start, t => t.End)
+                .Subscribe(_ => { RerunProcessor(); });
         }
     }
 }
