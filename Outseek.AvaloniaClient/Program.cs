5e61c990c453f40d3e427c58a75c16e5973656e0
felk <de.felix.koenig@gmail.com>
2021-05-13 14:07:46 +0200

    attach console (make stdout/stderr visible in output)

--- Outseek.AvaloniaClient/Program.cs
+++ Outseek.AvaloniaClient/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using Avalonia;
 using Avalonia.ReactiveUI;
 
@@ -6,12 +7,18 @@ namespace Outseek.AvaloniaClient
 {
     class Program
     {
+        [DllImport("kernel32", SetLastError = true)]
+        private static extern bool AttachConsole(int dwProcessId);
+
         // Initialization code. Don't use any Avalonia, third-party APIs or any
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
         // yet and stuff might break.
         [STAThread] // required for drag&drop of external files, see https://github.com/AvaloniaUI/Avalonia/issues/2635
-        public static void Main(string[] args) => BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
+        public static int Main(string[] args)
+        {
+            AttachConsole(-1);
+            return BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+        }
 
         // Avalonia configuration, don't remove; also used by visual designer.
         public static AppBuilder BuildAvaloniaApp()
